8648019388ed3b4a588b6d19941f593371c5f73d
Manuel Bartels <m.bartels@me-company.de>
2017-09-08 18:02:40 +0200

    postinstall make js dirs

--- postinstall.sh
+++ postinstall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+mkdir -p ./src/js/libs/first
+mkdir -p ./src/js/singles
 cp node_modules/babel-polyfill/dist/polyfill.min.js src/js/singles/polyfill.js
 cp node_modules/jquery/dist/jquery.min.js src/js/libs/first/jquery.js
