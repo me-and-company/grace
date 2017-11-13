db86fdd17da2915b490e7e8cf465184d8ea79e13
Manuel Bartels <m.bartels@me-company.de>
2017-11-13 11:00:24 +0100

    postinstall: brew install libpng

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ -f "Brewfile" ] && [ "$(uname -s)" = "Darwin" ]; then
+  brew bundle check >/dev/null 2>&1  || {
+    echo "==> Installing Homebrew dependencies…"
+    brew bundle
+  }
+fi
+
 mkdir -p ./src/js/libs/first
 mkdir -p ./src/js/singles
 cp node_modules/babel-polyfill/dist/polyfill.min.js src/js/singles/polyfill.js
