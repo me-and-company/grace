77198a89dad73d0dae3aec5eb5fdcc4f422e418e
Manuel <git@manuelbartels.de>
2018-01-17 11:55:54 +0100

    postinstall.sh -> postinstall.js

---
+++ postinstall.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+fs.mkdir('./src/js/libs/', (err) => {
+  if (err && err.code !== 'EEXIST') throw err;
+});
+fs.mkdir('./src/js/libs/first/', (err) => {
+  if (err && err.code !== 'EEXIST') throw err;
+});
+fs.mkdir('./src/js/singles/', (err) => {
+  if (err && err.code !== 'EEXIST') throw err;
+});
+
+fs.copyFile('node_modules/jquery/dist/jquery.min.js', './src/js/libs/first/jquery.js', (err) => {
+  if (err) throw err;
+  console.log('jquery was copied to destination');
+});
+
+fs.copyFile('node_modules/babel-polyfill/dist/polyfill.min.js', './src/js/singles/polyfill.js', (err) => {
+  if (err) throw err;
+  console.log('babel-polyfill was copied to destination');
+});
--- postinstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p ./src/js/libs/first
-mkdir -p ./src/js/singles
-cp node_modules/babel-polyfill/dist/polyfill.min.js src/js/singles/polyfill.js
-cp node_modules/jquery/dist/jquery.min.js src/js/libs/first/jquery.js
